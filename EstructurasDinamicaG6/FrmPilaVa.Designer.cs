f07095962bf9ac0e7dcbb40e03a423b114841d40
ValcocerLopez <vmalcocer@uamv.edu.ni>
2025-10-15 10:20:14 -0600

    formulario diseño

---
+++ EstructurasDinamicaG6/FrmPilaVa.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace EstructurasDinamicaG6
+{
+    partial class FrmPilaVA
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblEdades = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "edad";
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(147, 22);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(100, 22);
+            this.tbEdad.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(297, 20);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // lblEdades
+            // 
+            this.lblEdades.AutoSize = true;
+            this.lblEdades.Location = new System.Drawing.Point(37, 77);
+            this.lblEdades.Name = "lblEdades";
+            this.lblEdades.Size = new System.Drawing.Size(54, 16);
+            this.lblEdades.TabIndex = 3;
+            this.lblEdades.Text = "edades";
+            // 
+            // FrmPilaVA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 450);
+            this.Controls.Add(this.lblEdades);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmPilaVA";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblEdades;
+    }
+}
+
--- EstructurasDinamicaG6/FrmPilaVa.cs
+++ EstructurasDinamicaG6/FrmPilaVa.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EstructurasDinamicaG6
 {
-    public partial class Form1 : Form
+    public partial class FrmPilaVA : Form
     {
-        public Form1()
+        public FrmPilaVA()
         {
             InitializeComponent();
         }
