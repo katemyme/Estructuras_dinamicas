f4aab72def6bdcff907943b044443a3c70ee8f79
Andre <andre.guido@uamv.edu.ni>
2025-10-17 10:36:46 -0600

    Ejercicio práctico sobre Pilas.

--- EstructurasDinamicaG6/Form1.Designer.cs
+++ EstructurasDinamicaG6/Form1.Designer.cs
@@ -31,43 +31,94 @@
             this.label1 = new System.Windows.Forms.Label();
             this.tbEdad = new System.Windows.Forms.TextBox();
             this.lblEdades = new System.Windows.Forms.Label();
+            this.btnSacar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.lblEdades2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Edad:";
             // 
             // tbEdad
             // 
-            this.tbEdad.Location = new System.Drawing.Point(70, 7);
+            this.tbEdad.Location = new System.Drawing.Point(47, 5);
+            this.tbEdad.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbEdad.Name = "tbEdad";
-            this.tbEdad.Size = new System.Drawing.Size(100, 26);
+            this.tbEdad.Size = new System.Drawing.Size(68, 20);
             this.tbEdad.TabIndex = 1;
-            this.tbEdad.TextChanged += new System.EventHandler(this.tbEdad_TextChanged);
+            this.tbEdad.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbEdad_KeyDown);
             // 
             // lblEdades
             // 
-            this.lblEdades.Location = new System.Drawing.Point(12, 49);
+            this.lblEdades.Location = new System.Drawing.Point(8, 32);
+            this.lblEdades.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEdades.Name = "lblEdades";
-            this.lblEdades.Size = new System.Drawing.Size(266, 35);
+            this.lblEdades.Size = new System.Drawing.Size(177, 23);
             this.lblEdades.TabIndex = 2;
             this.lblEdades.Text = "Edades";
             this.lblEdades.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // btnSacar
+            // 
+            this.btnSacar.Location = new System.Drawing.Point(132, 3);
+            this.btnSacar.Name = "btnSacar";
+            this.btnSacar.Size = new System.Drawing.Size(75, 23);
+            this.btnSacar.TabIndex = 3;
+            this.btnSacar.Text = "Sacar";
+            this.btnSacar.UseVisualStyleBackColor = true;
+            this.btnSacar.Click += new System.EventHandler(this.btnSacar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(213, 3);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(93, 23);
+            this.btnMostrar.TabIndex = 4;
+            this.btnMostrar.Text = "Mostrar Último";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(312, 3);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(87, 23);
+            this.btnOrdenar.TabIndex = 5;
+            this.btnOrdenar.Text = "Mayor a menor";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // lblEdades2
+            // 
+            this.lblEdades2.Location = new System.Drawing.Point(222, 32);
+            this.lblEdades2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEdades2.Name = "lblEdades2";
+            this.lblEdades2.Size = new System.Drawing.Size(177, 23);
+            this.lblEdades2.TabIndex = 6;
+            this.lblEdades2.Text = "Edades";
+            this.lblEdades2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // FrmPilaAG
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 374);
+            this.ClientSize = new System.Drawing.Size(495, 78);
+            this.Controls.Add(this.lblEdades2);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnSacar);
             this.Controls.Add(this.lblEdades);
             this.Controls.Add(this.tbEdad);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "FrmPilaAG";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -81,6 +132,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tbEdad;
         private System.Windows.Forms.Label lblEdades;
+        private System.Windows.Forms.Button btnSacar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Label lblEdades2;
     }
 }
 
--- EstructurasDinamicaG6/Form1.cs
+++ EstructurasDinamicaG6/Form1.cs
@@ -1,25 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EstructurasDinamicaG6
 {
     public partial class FrmPilaAG : Form
     {
+        Stack<int> pila = new Stack<int>();
         public FrmPilaAG()
         {
             InitializeComponent();
         }
 
-        private void tbEdad_TextChanged(object sender, EventArgs e)
+        private void Agregar()
         {
+            int edad = int.Parse(tbEdad.Text);
+            pila.Push(edad);
+            Mostrar();
+        }
+        private void Mostrar()
+        {
+            string msn = "";
+            foreach (int edad in pila)
+            {
+                msn += " " + edad + " |";
+            }
+            lblEdades.Text = msn;
+        }
 
+        private void tbEdad_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Agregar();
+                tbEdad.Clear();
+                tbEdad.Focus();
+                Mostrar();
+            }
+        }
+
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+            if (pila.Count > 0)
+            {
+                pila.Pop();
+                Mostrar();
+            }
+            else
+            {
+                MessageBox.Show("Pila Vacía.");
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if (pila.Count > 0)
+            {
+                MessageBox.Show($"{pila.Peek()}");
+            }
+            else
+            {
+                MessageBox.Show("Pila Vacía.");
+            }
+            
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        { 
+            Stack<int> pilaAux = new Stack<int>();
+            while (pila.Count > 0)
+            {
+                int temp = pila.Pop();
+                while (pilaAux.Count > 0 && pilaAux.Peek() > temp)
+                {
+                    pila.Push(pilaAux.Pop());
+                }
+                pilaAux.Push(temp);
+            }
+            pila = pilaAux;
+            
+            string msn = "";
+            foreach (int edad in pila)
+            {
+                msn += " " + edad + " |";
+            }
+            lblEdades2.Text = msn;
         }
     }
 }
